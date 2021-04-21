7a728e0d945542170bcfee61aca11dbe19f10d2c
Philip <philip.johnsen@live.no>
2021-04-21 19:45:54 +0200

    Now returns user to channel it was in

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import discord
 import os
 import time
 
-bot = commands.Bot(command_prefix = '$')
+bot = commands.Bot(command_prefix = '!')
 
 @bot.command(pass_context=True)
 async def check(ctx):
@@ -12,10 +12,10 @@ async def check(ctx):
 @bot.command(pass_context=True)
 async def gåut(ctx, member: discord.Member):
 
-    #channelID = member.voice_channel.id
+    channelID = member.voice.channel
 
     channel = discord.utils.get(ctx.guild.voice_channels, name='GÅ UT!')
-    channel2 = discord.utils.get(ctx.guild.voice_channels, name='Lounge')
+    channel2 = discord.utils.get(ctx.guild.voice_channels, name=f'{channelID}')
 
     while True:
         await member.edit(voice_channel=channel)
@@ -24,10 +24,6 @@ async def gåut(ctx, member: discord.Member):
         await member.edit(mute=False)
         await member.edit(voice_channel=channel2)
         return False
-    
-
+     
 bot.run("NzcwMjg1NTU0MTkyNjEzNDE2.X5bWUw.oRXxJus_Q8hMJWeLr_cjfxmkt-k")
 
-
-
-#https://discord.com/api/oauth2/authorize?client_id=770285554192613416&permissions=29427712&scope=bot
